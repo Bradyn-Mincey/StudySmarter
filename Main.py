86e367b5634bc8052fa6c964a8e50b1dc2d90ae8
Bradyn-Mincey <bmmincey@student.fullsail.edu>
2026-05-15 17:10:02 -0400

    Wasnt finished with code on first commit

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ def create_task():
     due_date = input("Enter due date: ")
     priority = input("Enter priority (High/Medium/Low): ")
 
-    if not task:
+    if task.strip() == "":
         print("No task provided.")
         return
 
@@ -20,3 +20,45 @@ def create_task():
     tasks.append(task_data)
 
     print("Study task added!")
+
+
+def view_tasks():
+    if not tasks:
+        print("No tasks available.")
+        return
+
+    print("\n===== Study Tasks =====")
+
+    for i, task in enumerate(tasks):
+        print(f"""
+Task #{i+1}
+Subject: {task['subject']}
+Task: {task['task']}
+Due Date: {task['due_date']}
+Priority: {task['priority']}
+""")
+
+
+while True:
+
+    print("""
+===== StudySmarter =====
+1. Create Task
+2. View Tasks
+3. Exit
+""")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        create_task()
+
+    elif choice == "2":
+        view_tasks()
+
+    elif choice == "3":
+        print("Exiting StudySmarter...")
+        break
+
+    else:
+        print("Invalid option.")
